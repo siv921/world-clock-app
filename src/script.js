f29e4d9879424e8488e1ec2340145afc002c4b4c
siv921 <asivets@me.com>
2025-05-19 13:30:48 +0100

    added Javascript - initial time and date formating from moments.js

---
+++ src/script.js
@@ -0,0 +1,53 @@
+function addCityTime(event) {
+  let selectedCity = document.querySelector("#selected-city");
+  let selectedDate = document.querySelector("#selected-date");
+  let selectedTime = document.querySelector("#selected-time");
+  let localTimezone = moment.tz.guess();
+  if (event.target.value === "bangkok") {
+    selectedCity.innerHTML = `Bangkok`;
+    selectedDate.innerHTML = moment.tz("Asia/Bangkok").format("Do MMMM YYYY");
+    selectedTime.innerHTML = moment.tz("Asia/Bangkok").format("HH:mm:ss");
+  }
+  if (event.target.value === "reykjavik") {
+    selectedCity.innerHTML = `Reykjavik`;
+    selectedDate.innerHTML = moment
+      .tz("Atlantic/Reykjavik")
+      .format("Do MMMM YYYY");
+    selectedTime.innerHTML = moment.tz("Atlantic/Reykjavik").format("HH:mm:ss");
+  }
+  if (event.target.value === "copenhagen") {
+    selectedCity.innerHTML = `Copenhagen`;
+    selectedDate.innerHTML = moment
+      .tz("Europe/Copenhagen")
+      .format("Do MMMM YYYY");
+    selectedTime.innerHTML = moment.tz("Europe/Copenhagen").format("HH:mm:ss");
+  }
+  if (event.target.value === "local-time") {
+    selectedCity.innerHTML = localTimezone;
+    selectedTime.innerHTML = moment().format("HH:mm:ss");
+    selectedDate.innerHTML = moment().format("Do MMMM YYYY");
+  }
+}
+
+function showTime() {
+  let londonDate = document.querySelector("#date-london");
+  let newYorkDate = document.querySelector("#date-new-york");
+  let sydneyDate = document.querySelector("#date-sydney");
+  let londonTime = document.querySelector("#time-london");
+  let newYorkTime = document.querySelector("#time-new-york");
+  let sydneyTime = document.querySelector("#time-sydney");
+
+  londonDate.innerHTML = moment.tz("Europe/London").format("Do MMMM YYYY");
+  newYorkDate.innerHTML = moment.tz("America/New_York").format("Do MMMM YYYY");
+  sydneyDate.innerHTML = moment.tz("Australia/Sydney").format("Do MMMM YYYY");
+  londonTime.innerHTML = moment.tz("Europe/London").format("H:mm:ss");
+  newYorkTime.innerHTML = moment.tz("America/New_York").format("HH:mm:ss");
+  sydneyTime.innerHTML = moment.tz("Australia/Sydney").format("HH:mm:ss");
+}
+
+setInterval(showTime, 1000);
+
+setInterval(addCityTime, 1000);
+
+let selectCity = document.querySelector("#countries");
+selectCity.addEventListener("change", addCityTime);
