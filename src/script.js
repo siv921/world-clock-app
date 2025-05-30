499903dbea029338705542ddce89e5bcb22f861b
siv921 <asivets@me.com>
2025-05-30 12:17:10 +0100

    updated javascript

--- src/script.js
+++ src/script.js
@@ -3,28 +3,33 @@ function addCityTime(event) {
   let selectedDate = document.querySelector("#selected-date");
   let selectedTime = document.querySelector("#selected-time");
   let localTimezone = moment.tz.guess();
+
   if (event.target.value === "bangkok") {
     selectedCity.innerHTML = `Bangkok`;
     selectedDate.innerHTML = moment.tz("Asia/Bangkok").format("Do MMMM YYYY");
-    selectedTime.innerHTML = moment.tz("Asia/Bangkok").format("HH:mm:ss");
+    selectedTime.innerHTML = moment.tz("Asia/Bangkok").format("HH:mm");
   }
   if (event.target.value === "reykjavik") {
     selectedCity.innerHTML = `Reykjavik`;
     selectedDate.innerHTML = moment
       .tz("Atlantic/Reykjavik")
       .format("Do MMMM YYYY");
-    selectedTime.innerHTML = moment.tz("Atlantic/Reykjavik").format("HH:mm:ss");
+    selectedTime.innerHTML = moment.tz("Atlantic/Reykjavik").format("HH:mm");
   }
   if (event.target.value === "copenhagen") {
     selectedCity.innerHTML = `Copenhagen`;
     selectedDate.innerHTML = moment
       .tz("Europe/Copenhagen")
       .format("Do MMMM YYYY");
-    selectedTime.innerHTML = moment.tz("Europe/Copenhagen").format("HH:mm:ss");
+    selectedTime.innerHTML = moment.tz("Europe/Copenhagen").format("HH:mm");
   }
   if (event.target.value === "local-time") {
+    let selectedCity = document.querySelector("#selected-city");
+    let selectedDate = document.querySelector("#selected-date");
+    let selectedTime = document.querySelector("#selected-time");
+    let localTimezone = moment.tz.guess();
     selectedCity.innerHTML = localTimezone;
-    selectedTime.innerHTML = moment().format("HH:mm:ss");
+    selectedTime.innerHTML = moment().format("HH:mm");
     selectedDate.innerHTML = moment().format("Do MMMM YYYY");
   }
 }
@@ -46,8 +51,6 @@ function showTime() {
 }
 
 setInterval(showTime, 1000);
-
-setInterval(addCityTime, 1000);
 
 let selectCity = document.querySelector("#countries");
 selectCity.addEventListener("change", addCityTime);
